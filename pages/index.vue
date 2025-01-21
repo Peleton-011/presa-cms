4dca2951e1af3fdca9f1c0ed50550498c79e03e8
Zhuo <nicodiazan@gmail.com>
2025-01-21 12:42:38 +0100

    Move index to pages folder

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Welcome Back!</h1>
+      <p>You are signed in.</p>
+    </div>
+  </template>
+  
+  <script>
+  import { useAuth } from '@clerk/vue';
+  
+  export default {
+    setup() {
+      const { user } = useAuth();
+  
+      return { user };
+    },
+  };
+  </script>
+  
