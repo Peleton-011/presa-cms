5a1b1a80522ff1355672d82758d96a0dab4bf754
Zhuo <nicodiazan@gmail.com>
2025-01-30 14:47:55 +0100

    Update sign-in and sign-up pages to adapt to the new model

--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -195,197 +195,141 @@ const handleVerification = async () => {
 		showToast("Error during verification. Please try again.", "error");
 	}
 };
+
+definePageMeta({
+	layout: "auth-layout",
+});
 </script>
 
 <template>
-	<div v-if="isLoaded">
-		<div
-			class="min-h-screen grid md:grid-cols-5 bg-cover"
-			style="background-image: url('/images/background.jpg')"
+	<AuthLayout title="Let's do this!" v-if="isLoaded">
+		<form
+			@submit.prevent="handleSignUp"
+			class="flex flex-col gap-4"
+			v-if="!pendingVerification"
 		>
-			<ToastContainer />
+			<legend class="text-xl font-bold text-crimson">
+				Let's do this!
+			</legend>
 
-			<!-- Sidebar -->
-			<aside
-				class="md:col-span-2 flex flex-col justify-between items-center py-16 px-4 bg-black/75"
+			<div
+				class="grid md:grid-cols-2 gap-4 p-4 bg-black/25 backdrop-blur-sm"
 			>
 				<div
-					class="w-full flex flex-row flex-nowrap justify-center items-center"
+					v-for="field in formFields"
+					:key="field.id"
+					class="flex flex-col gap-2"
 				>
-					<!-- Using standard size classes instead of clamp -->
-					<img
-						src="/images/NERV-logo.png"
-						alt="NERV Logo"
-						class="w-20 md:w-40 lg:w-44 h-auto m-12"
-					/>
-					<div class="flex flex-col">
-						<!-- Using standard text size classes -->
-						<p
-							class="font-bold text-fluidLarge border-b-2 border-[#e31e24] text-nerv"
-						>
-							NERV
-						</p>
-						<p class="w-full text-xs md:text-sm mt-2">
-							To protect. To attack. To explore.
-						</p>
-					</div>
-				</div>
-				<footer class="text-xs md:text-sm text-tnr">
-					Under the supervision of
-					<a
-						href="https://www.un.org/"
-						target="_blank"
-						class="text-[#f18f01] hover:text-[#ebebd3] active:text-[#e31e24] transition-colors"
-					>
-						the UN
-					</a>
-				</footer>
-			</aside>
-
-			<!-- Main Content -->
-			<main
-				class="md:col-span-3 p-8 bg-black/50 backdrop-blur-md flex flex-col gap-8"
-			>
-				<blockquote
-					class="pl-6 border-l-4 border-[#e31e24] text-2xl italic text-tnr"
-				>
-					Extreme situations call for extreme measures.<br />
-					Do what you know is right.
-				</blockquote>
-
-				<form
-					@submit.prevent="handleSignUp"
-					class="flex flex-col gap-4"
-					v-if="!pendingVerification"
-				>
-					<legend class="text-xl font-bold text-crimson">
-						Let's do this!
-					</legend>
-
-					<div
-						class="grid md:grid-cols-2 gap-4 p-4 bg-black/25 backdrop-blur-sm"
-					>
-						<div
-							v-for="field in formFields"
-							:key="field.id"
-							class="flex flex-col gap-2"
-						>
-							<label :for="field.id" class="text-[#e31e24]">
-								{{ field.label }}
-							</label>
-							<div class="relative">
-								<input
-									:type="
-										field.type === 'password'
-											? passwordVisibility[field.name]
-												? 'text'
-												: 'password'
-											: field.type
-									"
-									:name="field.name"
-									:id="field.id"
-									v-model="form[field.name]"
-									:required="field.required"
-									:placeholder="field.placeholder"
-									@input="handleInput(field)"
-									@blur="handleBlur(field)"
-									class="w-full bg-[#071013] text-[#ebebd3] border border-white/20 p-2 rounded focus:outline-none focus:border-[#e31e24] transition-colors"
-									:class="{
-										'border-[#f18f01]':
-											fieldValidation[field.name] ===
-											false,
-										shake: shouldShake[field.name],
-									}"
-								/>
-								<button
-									v-if="field.type === 'password'"
-									type="button"
-									@click="
-										togglePasswordVisibility(field.name)
-									"
-									class="absolute right-2 top-1/2 -translate-y-1/2 text-white/50 hover:text-white transition-colors"
-								>
-									<PiEyeFill
-										v-if="!passwordVisibility[field.name]"
-										class="w-5 h-5"
-									/>
-									<PiEyeClosed v-else class="w-5 h-5" />
-								</button>
-							</div>
-							<p
-								class="text-sm text-[#f18f01] transition-opacity duration-200"
-								:class="{
-									'opacity-100':
-										fieldValidation[field.name] === false,
-									'opacity-0':
-										fieldValidation[field.name] !== false,
-								}"
-							>
-								{{ field.validationHint }}
-							</p>
-						</div>
-
+					<label :for="field.id" class="text-[#e31e24]">
+						{{ field.label }}
+					</label>
+					<div class="relative">
+						<input
+							:type="
+								field.type === 'password'
+									? passwordVisibility[field.name]
+										? 'text'
+										: 'password'
+									: field.type
+							"
+							:name="field.name"
+							:id="field.id"
+							v-model="form[field.name]"
+							:required="field.required"
+							:placeholder="field.placeholder"
+							@input="handleInput(field)"
+							@blur="handleBlur(field)"
+							class="w-full bg-[#071013] text-[#ebebd3] border border-white/20 p-2 rounded focus:outline-none focus:border-[#e31e24] transition-colors"
+							:class="{
+								'border-[#f18f01]':
+									fieldValidation[field.name] === false,
+								shake: shouldShake[field.name],
+							}"
+						/>
 						<button
-							type="submit"
-							class="md:col-span-2 mt-4 px-4 py-2 bg-[#e31e24] text-[#071013] rounded border border-white/20 hover:border-transparent active:bg-[#071013] active:text-[#e31e24] active:border-[#e31e24] transition-all duration-200"
+							v-if="field.type === 'password'"
+							type="button"
+							@click="togglePasswordVisibility(field.name)"
+							class="absolute right-2 top-1/2 -translate-y-1/2 text-white/50 hover:text-white transition-colors"
 						>
-							Join Us
-						</button>
-						<p class="text-sm text-[#f18f01] text-center md:col-span-2">
-							Already have an account?
-							<NuxtLink to="/sign-in" class="text-[#e31e24]"
-								>Sign In</NuxtLink
-							>
-						</p>
-					</div>
-				</form>
-				<form
-					v-else
-					class="flex flex-col gap-4"
-					@submit.prevent="handleVerification"
-				>
-					<legend class="text-xl font-bold text-crimson">
-						Just one last step!
-					</legend>
-
-					<div
-						class="grid md:grid-cols-1 gap-4 p-4 bg-black/25 backdrop-blur-sm"
-					>
-						<div class="flex flex-col gap-2">
-							<label for="code" class="text-[#e31e24]">
-								Enter the validation code sent to your email
-							</label>
-							<input
-								type="text"
-								name="code"
-								id="code"
-								required="true"
-								placeholder="XXX XXX"
-								v-model="verificationCode"
-								class="w-full bg-[#071013] text-[#ebebd3] border border-white/20 p-2 rounded focus:outline-none focus:border-[#e31e24] transition-colors"
+							<PiEyeFill
+								v-if="!passwordVisibility[field.name]"
+								class="w-5 h-5"
 							/>
-							<p
-								class="text-sm text-[#f18f01] transition-opacity duration-200"
-								:class="{
-									' opacity-100': verificationError,
-									' opacity-0': !verificationError,
-								}"
-							>
-								Wrong code. Please try again.
-							</p>
-						</div>
-
-						<button
-							type="submit"
-							class="md:col-span-2 mt-4 px-4 py-2 bg-[#e31e24] text-[#071013] rounded border border-white/20 hover:border-transparent active:bg-[#071013] active:text-[#e31e24] active:border-[#e31e24] transition-all duration-200"
-						>
-							Verify
+							<PiEyeClosed v-else class="w-5 h-5" />
 						</button>
 					</div>
-				</form>
-			</main>
-		</div>
-	</div>
+					<p
+						class="text-sm text-[#f18f01] transition-opacity duration-200"
+						:class="{
+							'opacity-100':
+								fieldValidation[field.name] === false,
+							'opacity-0': fieldValidation[field.name] !== false,
+						}"
+					>
+						{{ field.validationHint }}
+					</p>
+				</div>
+
+				<button
+					type="submit"
+					class="md:col-span-2 mt-4 px-4 py-2 bg-[#e31e24] text-[#071013] rounded border border-white/20 hover:border-transparent active:bg-[#071013] active:text-[#e31e24] active:border-[#e31e24] transition-all duration-200"
+				>
+					Join Us
+				</button>
+				<p class="text-sm text-[#f18f01] text-center md:col-span-2">
+					Already have an account?
+					<NuxtLink to="/sign-in" class="text-[#e31e24]"
+						>Sign In</NuxtLink
+					>
+				</p>
+			</div>
+		</form>
+		<form
+			v-else
+			class="flex flex-col gap-4"
+			@submit.prevent="handleVerification"
+		>
+			<legend class="text-xl font-bold text-crimson">
+				Just one last step!
+			</legend>
+
+			<div
+				class="grid md:grid-cols-1 gap-4 p-4 bg-black/25 backdrop-blur-sm"
+			>
+				<div class="flex flex-col gap-2">
+					<label for="code" class="text-[#e31e24]">
+						Enter the validation code sent to your email
+					</label>
+					<input
+						type="text"
+						name="code"
+						id="code"
+						required="true"
+						placeholder="XXX XXX"
+						v-model="verificationCode"
+						class="w-full bg-[#071013] text-[#ebebd3] border border-white/20 p-2 rounded focus:outline-none focus:border-[#e31e24] transition-colors"
+					/>
+					<p
+						class="text-sm text-[#f18f01] transition-opacity duration-200"
+						:class="{
+							' opacity-100': verificationError,
+							' opacity-0': !verificationError,
+						}"
+					>
+						Wrong code. Please try again.
+					</p>
+				</div>
+
+				<button
+					type="submit"
+					class="md:col-span-2 mt-4 px-4 py-2 bg-[#e31e24] text-[#071013] rounded border border-white/20 hover:border-transparent active:bg-[#071013] active:text-[#e31e24] active:border-[#e31e24] transition-all duration-200"
+				>
+					Verify
+				</button>
+			</div>
+		</form>
+	</AuthLayout>
 	<div v-else>Loading...</div>
 </template>
 
