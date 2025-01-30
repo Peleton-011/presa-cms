9dd075262915f021fac80096eb32e639f6b6112d
Zhuo <nicodiazan@gmail.com>
2025-01-30 14:48:50 +0100

    Update clerk middleware, although it still seems to have some errors

--- middleware/clerk.ts
+++ middleware/clerk.ts
@@ -1,21 +1,20 @@
 // Sample clerk middleware
-import { clerkMiddleware } from '@clerk/nuxt/server'
+import { clerkMiddleware } from "@clerk/nuxt/server";
 
 export default clerkMiddleware((event) => {
-  const { has } = event.context.auth
-  const isInvoicesRoute = event.path.startsWith('/api/invoices')
-  const canCreateInvoices = has({
-    permission: 'org:invoices:create',
-  })
+	const { userId } = useAuth();
+	const isInvoicesRoute = event.path.startsWith("/");
 
-  // Check if user is accessing sensitive customer data
-  if (isInvoicesRoute) {
-    // Check if user has the required permission
-    if (!canCreateInvoices) {
-      throw createError({
-        statusCode: 403,
-        statusMessage: 'Unauthorized: Missing permission to create invoices',
-      })
-    }
-  }
-})
+	// Check if user is accessing sensitive customer data
+	if (isInvoicesRoute) {
+		// Check if user has the required permission
+		if (!userId.value) {
+			navigateTo("/sign-up");
+            // throw createError({
+			// 	statusCode: 403,
+			// 	statusMessage:
+			// 		"Unauthorized: Missing permission to create invoices",
+			// });
+		}
+	}
+});
